0be2dbcf985372d8051c288caf4f5a08e4bac6ce
Vitaly Kovalev <1176502+intsco@users.noreply.github.com>
2020-06-07 16:25:42 +0200

    feat: add proper baseline model
    * train-baseline-model.ipynb - model training
    and prediction
    * telesto/models.py - contains ClassificationModelBase
    * updated model.py and model.pt in example_model to use
    Resnet18
    * updated example_data/labels.tsv
    * added __init__.py to the model directories
    so it's possible to import modules from them when
    running code from the repo root

--- example_model/model.py
+++ example_model/model.py
@@ -1,12 +1,12 @@
 from typing import List
 
 import numpy as np
+from PIL import Image
 import torch
 import torch.nn as nn
-from PIL import Image
+import torchvision
+
 from telesto.models import ClassificationModelBase
-from torchvision import transforms
-from torchvision.models import squeezenet1_0
 
 CLASSES = ["healthy", "infected"]
 MODEL_PATH = 'model.pt'
@@ -25,20 +25,20 @@ def _convert_to_pil(array):
 
 
 class ClassificationModel(ClassificationModelBase):
-    def __init__(self):
-        super().__init__(classes=CLASSES, model_path=MODEL_PATH)
+    def __init__(self, model_path=MODEL_PATH):
+        super().__init__(classes=CLASSES, model_path=model_path)
         self.input_size = 224
-        self.data_transform = transforms.Compose(
+        self.data_transform = torchvision.transforms.Compose(
             [
-                transforms.Resize(self.input_size),
-                transforms.ToTensor(),
-                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+                torchvision.transforms.Resize(self.input_size),
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
             ]
         )
 
     def _load_model(self, model_path: str):
-        model = squeezenet1_0()
-        model.classifier[1] = nn.Conv2d(512, self.class_n, kernel_size=(1, 1), stride=(1, 1))
+        model = torchvision.models.resnet18()
+        model.fc = nn.Linear(512, self.class_n)
         model.num_classes = self.class_n
 
         with open(model_path, "rb") as fp:
