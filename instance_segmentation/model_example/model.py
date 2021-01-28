4abbef91a64c98396daa2f691de33baca306fb91
Vitaly Kovalev <1176502+intsco@users.noreply.github.com>
2021-01-28 20:52:22 +0100

    fix: instance segmentation model output format

--- instance_segmentation/model_example/model.py
+++ instance_segmentation/model_example/model.py
@@ -2,16 +2,16 @@ from typing import List
 
 import numpy as np
 
-from telesto.instance_segmentation import DataStorage, DetectionObject
+from telesto.instance_segmentation import SegmentationObject
 from telesto.instance_segmentation.model import SegmentationModelBase
 
 
 class SegmentationModel(SegmentationModelBase):
-    def __init__(self, storage: DataStorage):
-        super().__init__(classes=["fg", "bg"], model_path="", storage=storage)
+    def __init__(self):
+        super().__init__(classes=["fg", "bg"], model_path="")
 
     def _load_model(self, model_path: str):
         pass
 
-    def predict(self, input: np.ndarray) -> List[DetectionObject]:
-        return [DetectionObject(coords=[(0, 1), (0, 1)])]
+    def predict(self, input: np.ndarray) -> List[SegmentationObject]:
+        return [SegmentationObject(coords=[(0, 1), (0, 1)])]
